821d5bc52057bb75aff51cbce8da415d2cb9aac2
jungmyungjin <myung2667@gmail.com>
2023-05-17 20:37:28 +0900

    react 코드 client 폴더로 이동

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import App from "./components/App/App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {" "}
+    {/* 실제프로젝트에서 없어질 것*/}
     <App />
   </React.StrictMode>
 );
